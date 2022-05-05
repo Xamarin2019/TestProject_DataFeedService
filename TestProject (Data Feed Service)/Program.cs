4632b22911bdce77041b41af328967378957b4f4
Кирилл Белов <k.belov@mielta.ru>
2022-05-05 15:47:36 +0300

    Добавьте файлы проекта.

---
+++ TestProject (Data Feed Service)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestProject__Data_Feed_Service_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Project: Data Feed Service");
+        }
+    }
+}
