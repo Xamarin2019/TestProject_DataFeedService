08e5bb9febf5f23493e43c09044176bea5511e45
Кирилл Белов <k.belov@mielta.ru>
2022-05-05 17:56:30 +0300

    Unpacking of the data archive added .

--- TestProject (Data Feed Service)/Program.cs
+++ TestProject (Data Feed Service)/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
 
 namespace TestProject__Data_Feed_Service_
 {
     class Program
     {
+        static string zipPath = @"..\..\..\data.zip";
+        static string path = @"..\..\..\data";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Test Project: Data Feed Service");
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("The 'data' directory was found.", path);
+            }
+            else
+            {
+                ZipFile.ExtractToDirectory(zipPath, path);
+                Console.WriteLine("The data.zip extracted");
+            }
+
         }
     }
 }
