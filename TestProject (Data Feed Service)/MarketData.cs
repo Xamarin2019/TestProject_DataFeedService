4611d6e8800f0b71cd2d6fe4c75ad2da03650377
maui2021tmb@gmail.com <maui2021tmb@gmail.com>
2022-05-05 19:13:01 +0300

    Add data parser
    Improved archive search and extraction
    
    Delete unnecessary file

---
+++ TestProject (Data Feed Service)/MarketData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace TestProject__Data_Feed_Service_
+{
+	// Timestamp,Last,LastSize,TotalVolume,Bid,Ask,TickId,BasisForLast,TradeMarketCenter,TradeConditions
+	public class MarketData
+    {
+		//public DateTime TimeStamp { get; set; }
+		public string Last { get; set; }
+		public string LastSize { get; set; }
+		public string TotalVolume { get; set; }
+		public string Bid { get; set; }
+		public string Ask { get; set; }
+		public string TickId { get; set; }
+		public string BasisForLast { get; set; }
+		public string TradeMarketCenter { get; set; }
+		public string TradeConditions { get; set; }
+
+		public MarketData(string[] data)
+        {
+			////TimeStamp = DateTime.Parse(data[0]);
+			//Last		      = data[1];
+			//LastSize	      = data[2];
+			//TotalVolume       = data[3];
+			//Bid			      = data[4];
+			//Ask			      = data[5];
+			//TickId			  = data[6];
+			//BasisForLast      = data[7];
+			//TradeMarketCenter = data[8];
+			//TradeConditions   = data[9];
+
+			var dataEnumerator = data.GetEnumerator();
+			dataEnumerator.MoveNext();
+			foreach (var propertyInfo in GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+			{
+				propertyInfo.SetValue(this, dataEnumerator.Current);
+				dataEnumerator.MoveNext();
+			}
+
+		}
+
+	}
+}
--- TestProject (Data Feed Service)/Program.cs
+++ TestProject (Data Feed Service)/Program.cs
@@ -1,27 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace TestProject__Data_Feed_Service_
 {
     class Program
     {
         static string zipPath = @"..\..\..\data.zip";
-        static string path = @"..\..\..\data";
+        static string folderName = new Regex(@"(\w*)(?=\.zip)").Match(zipPath).ToString();
+        static Regex getFileName = new Regex(@"(\w*)(?=\.\w{3})");
+        static string path = @"..\..\..\";
 
         static void Main(string[] args)
         {
             Console.WriteLine("Test Project: Data Feed Service");
-            if (Directory.Exists(path))
+
+            if (Directory.Exists(path + folderName))
             {
-                Console.WriteLine("The 'data' directory was found.", path);
+                Console.WriteLine("The \"{0}\" directory was found.", folderName);
             }
             else
             {
-                ZipFile.ExtractToDirectory(zipPath, path);
-                Console.WriteLine("The data.zip extracted");
+                ZipFile.ExtractToDirectory(zipPath, @"..\..\..\");
+                Console.WriteLine("The \"{0}.zip\" extracted", folderName);
+            }
+
+            string[] fileEntries = Directory.GetFiles(path + folderName);
+            foreach (string fileName in fileEntries)
+            {
+                Console.WriteLine("Begin reading file {0}", getFileName.Match(fileName));
+
+                var strings = ReadCsvFile(fileName).Result;
+                var dictionary = ConvertCsvData(strings);
             }
 
         }
+
+
+        static async Task<string[]> ReadCsvFile(string fileName)
+        {
+            using (var reader = File.OpenText(fileName))
+            {
+                string[] lines = await File.ReadAllLinesAsync(fileName);
+
+                var data = lines.Skip(1);
+                var sorted = data.Select(line => new
+                {
+                    SortKey = DateTime.Parse(line.Split(',')[0]),
+                    Line = line
+                })
+                .OrderBy(x => x.SortKey)
+                .GroupBy(x => x.SortKey)
+                .Select(group => group.First().Line);
+                //.Select(x => x.Line);
+
+                return sorted.ToArray();
+            }
+        }
+
+        static SortedDictionary<DateTime, MarketData> ConvertCsvData(string[] data)
+        {
+            var dict = new SortedDictionary<DateTime, MarketData>();
+            foreach (var item in data)
+            {
+                var values = item.Split(',');
+                dict.Add(DateTime.Parse(values[0]), new MarketData(values));
+            }
+
+            return dict;
+        }
     }
 }
+
